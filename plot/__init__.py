f398b53f9ca579b971ae152c27eb08a3a1bcd8a8
Sabrina Appel <s.appel@gsi.de>
2017-12-11 17:18:03 +0100

    Python classes  for plotting the accelerator lattices

---
+++ plot/__init__.py
@@ -0,0 +1,8 @@
+## \namespace orbit::matrix_lattice
+## \brief Python classes  for plotting the accelerator lattices
+##
+from PlotBeamline import PlotBeamline
+
+__all__ = []
+__all__.append("PlotBeamline")
+
